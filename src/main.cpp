1d7e293ec751b4b2b524aaaac8181d89e626c77d
Francisco Dias <franciscopdneto@gmail.com>
2019-10-09 22:05:45 -0300

    starting mpi multiprocess implementation.

--- src/main.cpp
+++ src/main.cpp
@@ -20,12 +20,20 @@ int main(int argc, char** argv) {
 
     int elementsLenght;
     std::cin >> elementsLenght;
-    MPI_Bcast(&elementsLenght, 1, MPI_INT, MAINPROC, MPI_COMM_WORLD);
     const int elementsPerProcLenght = elementsLenght / numProcessors;
 
     std::vector<float> elements(elementsLenght);
     for (int i = 0; i < elementsLenght; i++)
       std::cin >> elements[i];
+    
+    for (int dest = 1; dest < numProcessors; dest++) {
+      MPI_Send(&elementsLenght, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
+      for (int i = elementsPerProcLenght*(dest -1); i - elementsPerProcLenght*dest < 
+        + elementsPerProcLenght; i++) {
+
+        MPI_Send(&elements[i], 1, MPI_FLOAT, dest, 0, MPI_COMM_WORLD);
+      }
+    }
 
     clock_t tStart = clock();
 
@@ -48,7 +56,29 @@ int main(int argc, char** argv) {
 
   } else {
     int elementsLenght;
-    MPI_Bcast(&elementsLenght, 1, MPI_INT, MAINPROC, MPI_COMM_WORLD);
+    MPI_Recv(&elementsLenght, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+    std::vector<float> elements;
+    for (int i = 0; i < elementsLenght; i++) {
+      MPI_Recv(&elements[i], 1, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+
+    while(elements.size() > 1) {
+      const float operand = elements.back();
+      elements.pop_back();
+
+      MPI_Send(&operand, 1, MPI_FLOAT, myRank+1, 0, MPI_COMM_WORLD);
+    }
+    // Só sendo garantido que o número elementos é igual entre os processos.
+    for (int i = 0; i < elementsLenght -1; i++) {
+      const float operand1 = elements.back();
+      elements.pop_back();
+
+      float operand2;
+      MPI_Recv(&operand2, 1, MPI_FLOAT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+      elements.push_back( operand1 + operand2);
+    }
   }
 
   MPI_Finalize();
